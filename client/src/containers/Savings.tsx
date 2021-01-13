6a2c31537593c16724fd8327171427762fdda8cc
Borja Leiva <7519952+borjadotai@users.noreply.github.com>
2021-01-13 12:50:21 +0100

    :tada: Initial commit with basic functionality

--- client/src/containers/Savings.tsx
+++ client/src/containers/Savings.tsx
@@ -1,41 +1,89 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Heading, VStack } from '@chakra-ui/react'
 import Input from '../components/Input'
 import Slider from '../components/Slider'
 import LineChart from '../components/LineChart'
 import DefaultLayout from '../components/layouts/Default'
 
-// Note: This is just for example purposes
-// should be replaced with real data from the server
-const tempData = {
-    xAxis: [0, 1, 2, 3, 4, 5],
-    yAxis: [100, 150, 180, 210, 240, 350],
+type StateType = {
+    initialSavings: number | null
+    monthlyDeposits: number | null
+    interestRate: number | null
 }
 
-const Savings = () => (
-    <DefaultLayout>
-        <Container pt={6}>
-            <VStack spacing={4}>
-                <Heading as="h1">Interest Rate Calculator</Heading>
-                <Input label="Initial Savings amount" name="Initial Savings" placeholder="5000" />
-                <Input label="Monthly Deposit" name="Monthly Deposit" placeholder="100" />
-                <Slider
-                    label="Interest Rate"
-                    name="Interest Rate"
-                    defaultValue={2}
-                    min={0}
-                    max={15}
-                />
-                <LineChart
-                    title="Savings Over time"
-                    xAxisData={tempData.xAxis}
-                    yAxisData={tempData.yAxis}
-                    xLabel="Years"
-                    yLabel="Amount"
-                />
-            </VStack>
-        </Container>
-    </DefaultLayout>
-)
+enum ValueTypes {
+    InitialSavings = 'initialSavings',
+    MonthlyDeposits = 'monthlyDeposits',
+    InterestRate = 'interestRate',
+}
+
+const Savings = () => {
+    // Setting an initial default value to initialise the state and the UI
+    const defaultInterestRate = 2
+    // Piece of state for api received data with fake data fallback
+    const [data, setData] = useState<number[]>([100, 150, 180, 210, 240, 350])
+
+    const [state, setState] = useState<StateType>({
+        initialSavings: null,
+        monthlyDeposits: null,
+        interestRate: defaultInterestRate,
+    })
+
+    const handleChange = (type: ValueTypes, val: number) =>
+        state[type] !== val && setState({ ...state, [type]: val })
+
+    useEffect(() => {
+        fetch(
+            `http://localhost:3001?initialSavings=${state.initialSavings}&monthlyDeposits=${state.monthlyDeposits}&interestRate=${state.interestRate}`
+        )
+            .then((response) => response.json())
+            .then((data) =>
+                Array.isArray(data)
+                    ? setData(data)
+                    : console.log('Oh boy, something went wrong with the api calculations!')
+            )
+    }, [state])
+
+    return (
+        <DefaultLayout>
+            <Container pt={6}>
+                <VStack spacing={4}>
+                    <Heading as="h1">Interest Rate Calculator</Heading>
+                    <Input
+                        label="Initial Savings amount"
+                        name="Initial Savings"
+                        placeholder="5000"
+                        onChange={(e) =>
+                            handleChange(ValueTypes.InitialSavings, parseInt(e.target.value))
+                        }
+                    />
+                    <Input
+                        label="Monthly Deposit"
+                        name="Monthly Deposit"
+                        placeholder="100"
+                        onChange={(e) =>
+                            handleChange(ValueTypes.MonthlyDeposits, parseInt(e.target.value))
+                        }
+                    />
+                    <Slider
+                        label="Interest Rate"
+                        name="Interest Rate"
+                        defaultValue={defaultInterestRate}
+                        min={0}
+                        max={15}
+                        onChangeEnd={(val) => handleChange(ValueTypes.InterestRate, val)}
+                    />
+                    <LineChart
+                        title="Savings Over time"
+                        xAxisData={Array.from({ length: 50 }, (_, i) => i + 1)}
+                        yAxisData={data}
+                        xLabel="Years"
+                        yLabel="Amount"
+                    />
+                </VStack>
+            </Container>
+        </DefaultLayout>
+    )
+}
 
 export default Savings
--- server.ts
+++ server.ts
@@ -2,12 +2,74 @@ import express from "express";
 
 const app = express();
 
+app.use(function (req, res, next) {
+  if (req.headers.origin) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,Authorization"
+    );
+    res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+    if (req.method === "OPTIONS") return res.send(200);
+  }
+  next();
+});
+
 app.set("port", process.env.PORT || 3001);
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// type RequestType = {
+//   initialSavings: number | null
+//   monthlyDeposits: number | null
+//   interestRate: number | null
+// }
+
+const calculateSavings = (
+  initialSavings: any,
+  monthlyDeposits: any,
+  interestRate: any
+) => {
+  let i = 0;
+  let lastValue = 0;
+  let yearlyAmounts = [];
+  let result;
+  while (i <= 50) {
+    lastValue != 0
+      ? (result = (lastValue + monthlyDeposits * 12) * (1 + interestRate / 10))
+      : (result =
+          (initialSavings + monthlyDeposits * 12) * (1 + interestRate / 10));
+    yearlyAmounts.push(result);
+    lastValue = result;
+    i++;
+  }
+  return yearlyAmounts;
+};
+
+app.get("/", (req, res) => {
+  // Extract amounts from request
+  const { initialSavings, monthlyDeposits, interestRate } = req.query;
+  // Parse them into numbers
+  const parsedInitialSavings =
+    typeof initialSavings === "string" && parseInt(initialSavings);
+  const parseMonthlyDeposits =
+    typeof monthlyDeposits === "string" && parseInt(monthlyDeposits);
+  const parseInterestRate =
+    typeof interestRate === "string" && parseInt(interestRate);
+
+  // Calculate savings over time
+  const savingsOverTime = calculateSavings(
+    parsedInitialSavings,
+    parseMonthlyDeposits,
+    parseInterestRate
+  );
+
+  // Send savings over time
+  res.send(JSON.stringify(savingsOverTime));
+});
 
 app.listen(app.get("port"), () => {
   console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
